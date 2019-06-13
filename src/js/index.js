249a6b083bf4a5b1de437d891dd1fbba15be9d2d
iamsuzeet <iamsuzeet@gmail.com>
2019-06-13 19:34:49 +0545

    first lecture
    1.using es6 modules,default and named exports and imports 2. using real real world api using keys method using async await and using axios for AJAX

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,18 @@
-// Global app controller
-import x from './test';
+import axios from 'axios';
 
-console.log(x);
+async function getResults(query){
+  try{
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '153f3e4c1af106c7faad783be7c02376';
+    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes
+    console.log(recipes);
+  } catch(error){
+    alert(error);
+;  }
+
+}
+
+getResults('chicken wings');
+
+// https://food2fork.com/api/search
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const htmlWebpackPlugin =  require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['@babel/polyfill','./src/js/index.js'],
     output: {
-      path: path.resolve(__dirname, 'dist/js'),
-      filename: 'app.bundle.js'
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/app.bundle.js'
+    },
+    devServer: {
+      contentBase: './dist'
+    },
+    plugins: [
+      new htmlWebpackPlugin({
+          filename: 'index.html',
+          template: './src/index.html'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test:'/\.js?$/',
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
     }
 }
