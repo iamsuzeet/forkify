d90f0ccfe093822a2c62d7f88ef6bcc7ffc72e68
iamsuzeet <iamsuzeet@gmail.com>
2019-06-21 13:38:35 +0545

    created new recipe view and updated recipe model js

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -11,7 +11,7 @@ export default class Recipe {
       const res = await axios(`${proxy}https://www.food2fork.com/api/get/?key=${key}&rId=${this.id}`);
       // console.log(res.data.recipe.ingredients);
       this.title = res.data.recipe.title;
-      this.author - res.data.recipe.publisher;
+      this.author = res.data.recipe.publisher;
       this.img = res.data.recipe.image_url;
       this.url = res.data.recipe.source_url;
       this.ingredients = res.data.recipe.ingredients;
@@ -35,6 +35,7 @@ export default class Recipe {
   parseIngredients(){
     const unitsLong = ['tablespoons','tablespoon','ounces','ounce','teaspoons','teaspoon','cups','pounds'];
     const unitsShort = ['tbsp','tbsp','oz','oz','tsp','tsp','cup','pound'];
+    const units = [...unitsShort, 'kg','g'];
     const newIngredients = this.ingredients.map(el => {
         
         // 1) Uniform units
@@ -48,7 +49,7 @@ export default class Recipe {
 
         // 3) Parse ingredients into count,unit and ingredient
         const arrIng = ingredient.split(' ');
-        const unitIndex = arrIng.findIndex(e12 => unitsShort.includes(e12));
+        const unitIndex = arrIng.findIndex(e12 => units.includes(e12));
         // console.log(unitsShort);
 
         let objIng;
@@ -85,9 +86,24 @@ export default class Recipe {
             ingredient
           }
         }
-
+        // console.log(objIng);
         return objIng;
+        
     });
     this.ingredients = newIngredients;
+    // console.log(this.ingredients);
+  }
+
+  updateServings(type){
+    //servings
+    const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
+
+    //ingredients
+    this.ingredients.forEach(ing => {
+      ing.count *= (newServings / this.servings);
+    })
+
+    this.servings = newServings;
+
   }
 }
